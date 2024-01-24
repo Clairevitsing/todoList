56eca0d80d204d98001966506e9ad79c742ae01d
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-01-25 00:02:50 +0100

    afficher la liste TODO

--- App.js
+++ App.js
@@ -2,12 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+   const sampleGoals = [
+    "Faire les courses",
+    "Aller à la salle de sport 3 fois par semaine",
+    "Monter à plus de 5000m d'altitude",
+    "Acheter mon premier appartement",
+    "Perdre 5 kgs",
+    "Gagner en productivité",
+    "Apprendre un nouveau langage",
+    "Faire une mission en freelance",
+    "Organiser un meetup autour de la tech",
+    "Faire un triathlon",
+   ];
+  
+   const listGoals = sampleGoals.map((goal) =>
+      <Text>{goal}</Text>
+   );
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h1}>Liste de goals</Text>
+      <Text style={styles.input }></Text>
+      <Text style={styles.button}>Add</Text>
+      
+      <Text style={styles.li}>{listGoals}</Text>
+      
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +40,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  h1: {
+    fontSize: '20px',
+  },
+  button: {
+  backgroundColor: '#04AA6D',
+  border: 'none',
+  color: 'white',
+  padding: '15px 32px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  fontSize: '16px',
+}
+
 });
+
+
+
+
